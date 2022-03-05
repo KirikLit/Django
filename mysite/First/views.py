23d5124a6ccc8257ba076b13810363d0f66c9ff7
KirikLit <vip.litvinov123@mail.ru>
2022-03-05 19:16:22 +0300

    Start

--- mysite/First/views.py
+++ mysite/First/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.views.generic import ListView
+from .models import Post
 
 
-def homePageView(request):
-    return HttpResponse('Hello, World!')
+class HomePageView(ListView):
+    model = Post
+    template_name = 'home.html'
+    context_object_name = 'all_posts_list'
